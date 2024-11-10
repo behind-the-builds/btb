86628200ab3a01f16f419e61692eae7f6c31be7e
behind-the-builds <norwell.llc@gmail.com>
2024-11-09 20:32:02 -0500

    Create btb.js

---
+++ btb.js
@@ -0,0 +1,29 @@
+function toggleAudio(event, audioId, iconId, progressBarId) {
+  event.preventDefault();
+  const audio = document.getElementById(audioId);
+  const playPauseIcon = document.getElementById(iconId);
+  const progressBar = document.getElementById(progressBarId);
+
+  if (audio.paused) {
+    audio.play();
+    playPauseIcon.classList.replace("fa-play", "fa-pause");
+  } else {
+    audio.pause();
+    playPauseIcon.classList.replace("fa-pause", "fa-play");
+  }
+
+  audio.ontimeupdate = () => {
+    const progress = (audio.currentTime / audio.duration) * 100;
+    progressBar.value = progress;
+  };
+
+  audio.onended = () => {
+    playPauseIcon.classList.replace("fa-pause", "fa-play");
+    progressBar.value = 0;
+  };
+
+  progressBar.addEventListener("input", (e) => {
+    const newTime = (e.target.value / 100) * audio.duration;
+    audio.currentTime = newTime;
+  });
+}
